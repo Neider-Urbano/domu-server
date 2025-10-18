b38e75d6bf93c1e0a669c047d2131c8a76897fd0
neider urbano <jjuly8julianur@gmail.com>
2025-10-17 23:03:00 -0500

    add env

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/domu-db");
+    await mongoose.connect(
+      process.env.DB_URL || "mongodb://localhost:27017/domu-db"
+    );
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err);
